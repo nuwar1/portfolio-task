d6325f97b23378e9ac6dd67235a0255ce950d1a8
nuwar1 <nuwarkurdi015@gmail.com>
2025-11-01 17:43:08 +0200

    create about section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Navbar from "./components/navbar/navbar.jsx";
 import Hero from "./components/hero/hero.jsx";
 import Portfolio from "./components/portfolio/portfolio.jsx";
+import About from "./components/about/about.jsx";
 function App(){
     return(
       <>
       <Navbar/>
       <Hero/>
       <Portfolio/>
+      <About/>
       </>
     )
 }
