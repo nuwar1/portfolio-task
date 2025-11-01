20e9a33f6fca5ddebea05767193511749a4be046
nuwar1 <nuwarkurdi015@gmail.com>
2025-11-01 19:02:54 +0200

    create footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from "./components/hero/hero.jsx";
 import Portfolio from "./components/portfolio/portfolio.jsx";
 import About from "./components/about/about.jsx";
 import Contact from "./components/contact/contact.jsx";
+import Footer from "./components/footer/footer.jsx";
 function App(){
     return(
       <>
@@ -11,6 +12,7 @@ function App(){
       <Portfolio/>
       <About/>
       <Contact/>
+      <Footer/>
       </>
     )
 }
