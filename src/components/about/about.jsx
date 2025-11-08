c74dd68a566895d7d44158a5501cdfb0ea961343
nuwar1 <nuwarkurdi015@gmail.com>
2025-11-08 21:31:38 +0200

    minor edit

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -9,7 +9,7 @@ function About() {
                     <div className="col-md-4"><p className="mb-0">Freelancer is a free bootstrap theme created by Start Bootstrap. The download includes the complete source files including HTML, CSS, and JavaScript as well as optional SASS stylesheets for easy customization.</p></div>
                     <div className="col-md-4"><p className="mb-0">You can create your own custom avatar for the masthead, change the icon in the dividers, and add your email address to the contact form to make it fully functional!</p></div>
                 </div>
-                <div class="text-center mt-4">
+                <div className="text-center mt-4">
                     <button className="btn btn-xl btn-outline-light">
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
